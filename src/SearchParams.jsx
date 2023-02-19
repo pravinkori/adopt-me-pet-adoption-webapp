719e5103832a1c822a6dcb3703622fcb3b5b3193
Pravin <pravinkori@protonmail.ch>
2023-02-20 00:55:00 +0530

    Added a hook to make form input mutable.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-    const location = "Seattle, WA";
+    const [location, setLocation] = useState("");
     return (
         <div className="search-params">
             <form action="">
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="location" />
+                    <input
+                        onChange={(e) => setLocation(e.target.value)}
+                        id="location"
+                        value={location}
+                        placeholder="Enter your location" />
                 </label>
                 <button>
                     Submit
