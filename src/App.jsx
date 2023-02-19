ed265eeba449bcce0f3f59afc3576f9c180d64c7
Pravin <pravinkori@protonmail.ch>
2023-02-19 23:39:36 +0530

    Removed unused code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Pet from './Pet';
 import SearchParams from "./SearchParams";
 
 /*
@@ -12,30 +11,8 @@ const App = () => {
         <div>
             <h1>Adopt Me!</h1>
             <SearchParams />
-            {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-            <Pet name="Winter" animal="Cat" breed="Mixed" /> */}
         </div>
     )
-
-    // return React.createElement("div", {}, [
-    //     React.createElement("h1", {}, "Adopt Me"),
-    //     React.createElement(Pet, {
-    //         name: "Luna",
-    //         animal: "Dog",
-    //         breed: "Havanese",
-    //     }),
-    //     React.createElement(Pet, {
-    //         name: "Pepper",
-    //         animal: "Bird",
-    //         breed: "Cockatiel",
-    //     }),
-    //     React.createElement(Pet, {
-    //         name: "Winter",
-    //         animal: "Cat",
-    //         breed: "Mixed",
-    //     }),
-    // ]);
 };
 
 const container = document.getElementById("root");
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,13 +1,4 @@
-// import React from "react";
-
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//         React.createElement("h2", {}, props.breed),
-//     ]);
-// };
+import React from "react";
 
 const Pet = (props) => {
     return (
