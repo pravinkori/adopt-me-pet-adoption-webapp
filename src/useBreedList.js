ba3e57f96c5399c9ce7330662668a7a2ec7e4dde
Pravin <pravinkori@protonmail.ch>
2023-03-02 20:58:20 +0530

    Refactored fetching of breed list using React Query.

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreed from "./fetchBreedList";
 
 // Custom hook
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded");
+    const results = useQuery(["breeds", animal], fetchBreed);
+    // const [status, setStatus] = useState("unloaded");
 
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal]);
-        } else {
-            requestBreedList();
-        }
+    // useEffect(() => {
+    //     if (!animal) {
+    //         setBreedList([]);
+    //     } else if (localCache[animal]) {
+    //         setBreedList(localCache[animal]);
+    //     } else {
+    //         requestBreedList();
+    //     }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
+    //     async function requestBreedList() {
+    //         setBreedList([]);
+    //         setStatus("loading");
 
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            );
+    //         const res = await fetch(
+    //             `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    //         );
 
-            const json = await res.json();
-            localCache[animal] = json.breeds || []
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
+    //         const json = await res.json();
+    //         localCache[animal] = json.breeds || []
+    //         setBreedList(localCache[animal]);
+    //         setStatus("loaded");
 
-        }
-    }, [animal]);
+    //     }
+    // }, [animal]);
 
-    return [breedList, status];
+    // Following LOC returns empty array if result is not ready.
+    return [results?.data?.breeds ?? [], results.status];
 }
